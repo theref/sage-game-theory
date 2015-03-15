c5f8f122b5c261c2f1c4135e1c0ab8c298add995
James Campbell <james.campbell@tanti.org.uk>
2015-03-15 10:47:46 -0700

    creates named_games.py

---
+++ src/sage/game_theory/named_games.py
@@ -0,0 +1,9 @@
+r"""
+Named Normal Form Games.
+
+A module containing several standard Normal Form Games that are standard in
+literature. A list of all Normal Form Games in this database is available via tab
+completion. Type ``NormalFormGame.`` (include the last dot) and hit
+the ``tab`` key to see which Normal Form Games are available.
+"""
+from sage.NormalFormGame.catalog import *
