e2e240380e60ec84c44d24eaa8a8b6c59436d829
vince <vincent.knight@gmail.com>
2015-03-15 10:50:37 -0700

    Writing basic PD

---
+++ src/sage/game_theory/catalog.py
@@ -0,0 +1,16 @@
+"""
+Documentation for the game theory catalog
+"""
+from sage.game_theory import NormalFormGame
+from sage import matrix
+
+def PrisonersDilemma():
+    """
+    Return a Prisoners Dilemma game
+
+        sage: g = PrisonersDilemma()
+        sage: g
+    """
+    A = matrix([[2,5],[0,4]])
+    g = NormalFormGame([A, A.transpose()])
+    return g
