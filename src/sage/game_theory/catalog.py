dcc5a77b4bb342cccbf592d32f775534fc2be899
vince <vincent.knight@gmail.com>
2015-03-15 11:27:22 -0700

    Fixing statement in test

--- src/sage/game_theory/catalog.py
+++ src/sage/game_theory/catalog.py
@@ -8,7 +8,7 @@ def PrisonersDilemma():
     """
     Return a Prisoners Dilemma game
 
-        sage: g = game_theory.catalog.PrisonersDilemma()
+        sage: g = game_theory.PrisonersDilemma()
         sage: g
         Normal Form Game with the following utilities: {(0, 1): [5, 0], (1, 0): [0, 5], (0, 0): [2, 2], (1, 1): [4, 4]}
     """
